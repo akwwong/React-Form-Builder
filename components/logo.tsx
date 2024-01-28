bc14450ea692467c88c5af5532f9ffd63045dbc3
Alex Wong <akwong14@myseneca.ca>
2024-01-28 15:48:02 -0500

    added stylization

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 function Logo() {
-  return <div>logo</div>;
+  return (
+    <Link
+      href={"/"}
+      className="font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer"
+    >
+      Placeholder
+    </Link>
+  );
 }
 
 export default Logo;
